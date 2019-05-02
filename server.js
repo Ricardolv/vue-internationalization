2150293fe01a4f0526d14ec71be939cdc4777df9
Ricardolv <richardluizv@gmail.com>
2019-05-02 19:48:17 -0300

    add js

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ import express from 'express';
 import path from 'path';
 import serveStatic from 'serve-static';
 
+const express = require('express');
+const serveStatic = require("serve-static")
+const path = require('path');
+
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
-
-var port = process.env.PORT || 5000;
-
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
 app.listen(port);
+
 console.log('server started '+ port);
